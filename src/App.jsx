dae013ac81c9050806cb68acf9a8033cdaef7814
ElioSpena <elio.spena.94@gmail.com>
2025-11-13 15:02:28 +0100

    feat: creato filtro per generi

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,52 @@
-import films from "./assets/data/films"
+import { useEffect, useState } from "react"
+import filmsList from "./assets/data/filmsList"
 
 function App() {
+const [films, setFilms] = useState(filmsList);
+const [genre, setGenre] = useState("");
+console.log(genre);
+
+
+useEffect( () => {
+  genre === "" ? setFilms(filmsList)
+   : 
+ setFilms(filmsList.filter((curFilm) => curFilm.genre.includes(genre)));
+},
+ [genre])
+  
   return (
     <>
       <section>
 
+          <form>
+              <select
+              value={genre}
+              onChange={ (event)=>
+                setGenre(event.target.value)
+              }
+              name="films">
+                {filmsList.map((curFilm, index) => 
+                <option
+                value={curFilm.genre}
+                 key={index}>
+                  {curFilm.genre}
+                </option>
+                )}
+                <option value=""></option>
+              </select>
+        </form>
+
         <ul>
 
-            {films.map((curFilm) => 
-            <li>
+            {films.map((curFilm, index) => 
+            <li key={index}>
               <a href="">{curFilm.title}</a>
             </li>
             )}
 
         </ul>
 
-        <form action="">
-              <select name="film" id="">
-                {films.map((curFilm) => 
-                <option>{curFilm.genre}</option>
-                )}
-              </select>
-        </form>
+      
 
 
       </section>
